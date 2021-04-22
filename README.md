651afa04a9b1b39addcac142542fe2b48bb6e4a5
Damien Heulin <damienheulin87@gmail.com>
2021-04-22 15:37:13 +0200

    Vue 3 Forms - Basic Input

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# vue-mastery
-Mon avancée sur les cours Vue 3 de Vue Mastery
+# Vue Mastery
+> Mon avancée sur les cours Vue 3 de Vue Mastery# Three.js Journey
+
+![Three.js logo](./docs/vuemastery.svg)
+
+## Meta
+Damien Heulin – [Twitter](https://twitter.com/damien_hl) – [Mail](mailto:damienheulin87@gmail.com) – [Github](https://github.com/damien-hl)
---
+++ vue-3-forms/src/App.vue
@@ -0,0 +1,130 @@
+<template>
+  <div>
+    <h1>Create an event</h1>
+    <form>
+      <label>Select a category</label>
+      <select v-model="event.category">
+        <option
+          v-for="option in categories"
+          :value="option"
+          :key="option"
+          :selected="option === event.category"
+        >
+          {{ option }}
+        </option>
+      </select>
+
+      <h3>Name & describe your event</h3>
+
+      <base-input v-model="event.title" label="Title" type="text" />
+
+      <base-input v-model="event.description" label="Description" type="text" />
+
+      <h3>Where is your event?</h3>
+
+      <base-input v-model="event.location" label="Location" type="text" />
+
+      <h3>Are pets allowed?</h3>
+      <div>
+        <input type="radio" v-model="event.pets" :value="1" name="pets" />
+        <label>Yes</label>
+      </div>
+
+      <div>
+        <input type="radio" v-model="event.pets" :value="0" name="pets" />
+        <label>No</label>
+      </div>
+
+      <h3>Extras</h3>
+      <div>
+        <input type="checkbox" v-model="event.extras.catering" />
+        <label>Catering</label>
+      </div>
+
+      <div>
+        <input type="checkbox" v-model="event.extras.music" />
+        <label>Live music</label>
+      </div>
+
+      <button type="submit">Submit</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import BaseInput from "@/components/BaseInput.vue";
+
+export default {
+  name: "App",
+  components: {
+    BaseInput,
+  },
+  data() {
+    return {
+      categories: [
+        "sustainability",
+        "nature",
+        "animal welfare",
+        "housing",
+        "education",
+        "food",
+        "community",
+      ],
+      event: {
+        category: "",
+        title: "",
+        description: "",
+        location: "",
+        pets: 1,
+        extras: {
+          catering: false,
+          music: false,
+        },
+      },
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  margin: 0 auto;
+  max-width: 400px;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+
+label {
+  font-size: 80%;
+}
+
+input[type="checkbox"] {
+  margin-bottom: 1rem;
+}
+
+input[type="radio"] {
+  margin-bottom: 0.5rem;
+}
+
+select,
+.field {
+  margin-bottom: 1rem;
+  padding: 0.5rem;
+  box-sizing: border-box;
+  width: 100%;
+  display: block;
+  border: 1px solid rgb(196, 196, 196);
+  border-radius: 4px;
+}
+
+button[type="submit"] {
+  padding: 0.75rem 2rem;
+  background: linear-gradient(90deg, #41b782, #86d169);
+  border: none;
+  border-radius: 4px;
+  color: #fff;
+  cursor: pointer;
+}
+</style>
