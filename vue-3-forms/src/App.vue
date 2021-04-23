f2254dedaa17b4c660c629be8a48e0f9821b11fa
Damien Heulin <damienheulin87@gmail.com>
2021-04-23 10:02:02 +0200

    Vue 3 Forms - Basic Radio

--- vue-3-forms/src/App.vue
+++ vue-3-forms/src/App.vue
@@ -31,13 +31,11 @@
 
       <h3>Are pets allowed?</h3>
       <div>
-        <input type="radio" v-model="event.pets" :value="1" name="pets" />
-        <label>Yes</label>
+        <base-radio v-model="event.pets" :value="1" label="Yes" name="pets" />
       </div>
 
       <div>
-        <input type="radio" v-model="event.pets" :value="0" name="pets" />
-        <label>No</label>
+        <base-radio v-model="event.pets" :value="0" label="No" name="pets" />
       </div>
 
       <h3>Extras</h3>
